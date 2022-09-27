aba3f074a5e2413050daf319ad022f18e710915c
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-09-27 16:10:05 +0200

    Add response interceptor and redirect user to login on 401 response

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -1,13 +1,25 @@
 import axios from "axios";
 import store from "./store";
+import { useRouter } from 'vue-router'
 
 const axiosClient = axios.create({
   baseURL: 'http://localhost:8000/api'
 })
 
 axiosClient.interceptors.request.use(config => {
-    config.headers.Authorization = `Bearer ${store.state.user.token}`
-    return config;
-  })
+  config.headers.Authorization = `Bearer ${store.state.user.token}`
+  return config;
+})
+
+axiosClient.interceptors.response.use(response => {
+  return response;
+}, error => {
+  if (error.response.status === 401) {
+    const router = useRouter();
+    sessionStorage.removeItem('TOKEN')
+    router.push({name: 'Login'})
+  }
+  return error;
+})
 
 export default axiosClient;
