424016cdeb9a95364dacac08d83f1ec09324089c
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-09-27 16:25:18 +0200

    Make a request to get user info on load

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -33,9 +33,21 @@
 
               <!-- Profile dropdown -->
               <Menu as="div" class="ml-3 relative">
-                <div>
+                <div class="flex">
                   <MenuButton class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                     <span class="sr-only">Open user menu</span>
+                    <div class="mx-3">
+                      <div
+                        class="text-left text-base font-medium leading-none text-white"
+                      >
+                        {{ user.name }}
+                      </div>
+                      <div
+                        class="text-sm font-medium leading-none text-gray-400"
+                      >
+                        {{ user.email }}
+                      </div>
+                    </div>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       class="h-8 w-8"
@@ -54,7 +66,7 @@
                 </div>
                 <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                   <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <MenuItem v-slot="{ active }">
+                    <MenuItem v-slot="{}">
                       <a @click="logout" :class="['block px-4 py-2 text-sm text-gray-700 cursor-pointer']">
                         Sign out
                       </a>
@@ -168,6 +180,8 @@ export default {
           });
         });
     }
+
+    store.dispatch("getUser");
 
     return {
       user: computed(() => store.state.user.data),
